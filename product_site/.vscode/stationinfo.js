72a7da4bfa2e08b2bac02556d6265de4f569f984
emiliennevincens-lab <emilienne.vincens@uri.edu>
2025-10-25 18:53:11 -0400

    WK8 JS creation

---
+++ product_site/.vscode/stationinfo.js
@@ -0,0 +1,13 @@
+/*I am Emilienne Vincens and this is my JavaScript file. It is due on Sunday 26th of October.
+     This page will load all the info the users want to preview when they select a gas station. They have access to all the info to choose to go or go back to the list of the other gas stations. */
+
+var name = "Kingston Convenience Mart";
+var adress = "2360 Kingstown Rd, Kingston, RI 02881";
+var distance = 2.5; 
+var fuelPrice = 3.59
+var rating = 4.5; // out of 5
+
+var elements = document.getElementsByClassName('h3');
+elements[0].innerHTML = “Here is some new text for example ” ;
+
+
