7e28041e6267987545af458dd93600254d39484d
emiliennevincens-lab <emilienne.vincens@uri.edu>
2025-10-26 20:52:44 -0400

    wk8 final

--- product_site/js/stationinfo.js
+++ product_site/js/stationinfo.js
@@ -1,17 +1,36 @@
 /*I am Emilienne Vincens and this is my JavaScript file. It is due on Sunday 26th of October.
      This page will load all the info the users want to preview when they select a gas station. They have access to all the info to choose to go or go back to the list of the other gas stations. */
 
+console.log("JS file is connected!");  //just to check that the js file is properly linked to the html file
+
 var name = "Kingston Convenience Mart";
 var adress = "2360 Kingstown Rd, Kingston, RI 02881";
-var distance = 2.5; 
+var distance = 250; 
 var fuelPrice = 3.59;
-var rating = 4.5; // out of 5
+var rating = 4.5; // out of 5 
+var fuelType = "Regular";
 
-/*var elements = document.getElementsByClassName('p');
-elements[0].innerHTML = “Here is some new text for example ” ; */
+var element = document.getElementsByTagName('h3');
+element[0].innerHTML = name ;
 
-var element = document.getElementByClass('time');
-element.innerHTML = '<p>New content</p>';
+var element = document.getElementsByTagName('p');
+element[0].innerHTML = adress ;
+element[3].innerHTML = "Review : " + rating + " / 5 stars" ;
 
-document.write("Welcome to Cosmic Horoscopes! 🌟Discover your destiny today.")
-alert("Welcome to Cosmic Horoscopes! 🌟Discover your destiny today.")
+time=distance*1.5
+ // we took an average speed of 40mph in town so 1.5 min per mile
+ // this following line converts minute time in a hours and minuts format.
+if (time <= 60){ var dur = time + " mins to go"} else { var dur= (distance/1.5).toFixed(0)  + " hours & " +time%60 + " mins"} ;
+
+var element = document.getElementsByClassName('time');
+element[0].innerHTML = distance + " mi to go - " + dur;
+
+var element = document.querySelector('.price')
+//var element = document.getElementsByClassName('price');
+element.textContent = fuelType + " gas price = $"+ fuelPrice + "/gal";
+
+var imgElement = document.getElementsByClassName('imagetext');
+imgElement[0].src = "images/img_template.jpeg"; // for now it doesn't change the image but it will be useful in the future to adapt to each gas station
+
+
+
